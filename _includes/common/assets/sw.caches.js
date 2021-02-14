2f1cb479113473c1accf7c6dd07c884000a8e774
Gerard Tosserams <gtosserams@gmail.com>
2021-02-14 16:20:10 +0100

    Updates

--- _includes/common/assets/sw.caches.js
+++ _includes/common/assets/sw.caches.js
@@ -1,4 +1,4 @@
-self.addEventListener("activate", function (event) {
+/*self.addEventListener("activate", function (event) {
   const current = ["{{ theme_alias }}"];
 
   event.waitUntil(
@@ -37,3 +37,44 @@ self.addEventListener("fetch", function (e) {
     );
   }
 });
+*/
+// service-worker.js
+
+// set names for both precache & runtime cache
+workbox.core.setCacheNameDetails({
+  prefix: 'my-blog',
+  suffix: 'v1.0',
+  precache: 'precache',
+  runtime: 'runtime-cache'
+});
+
+// let Service Worker take control of pages ASAP
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+// let Workbox handle our precache list
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+// use `NetworkFirst` strategy for html
+workbox.routing.registerRoute(
+  /\.html$/,
+  new workbox.strategies.NetworkFirst()
+);
+
+// use `NetworkFirst` strategy for css and js
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  new workbox.strategies.NetworkFirst()
+);
+
+// use `CacheFirst` strategy for images
+workbox.routing.registerRoute(
+  /assets\/(img|icons)/,
+  new workbox.strategies.CacheFirst()
+);
+
+// use `StaleWhileRevalidate` third party files
+workbox.routing.registerRoute(
+  /^https?:\/\/cdn.staticfile.org/,
+  new workbox.strategies.StaleWhileRevalidate()
+);
